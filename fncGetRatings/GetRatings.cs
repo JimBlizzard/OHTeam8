b918a975926a26d20cc8c853404a7d72ce19b8db
John Ferrari <jferrari@microsoft.com>
2020-09-16 12:52:57 -0400

    working version of GetRatings

--- fncGetRatings/GetRatings.cs
+++ fncGetRatings/GetRatings.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace fncGetRatings
 {
@@ -14,21 +17,49 @@ namespace fncGetRatings
     {
         [FunctionName("GetRatings")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            // App settings
+            var dbConn = System.Environment.GetEnvironmentVariable("DBConnectionString", EnvironmentVariableTarget.Process);
+            var databaseId = System.Environment.GetEnvironmentVariable("DatabaseId", EnvironmentVariableTarget.Process);
+            var containerId = System.Environment.GetEnvironmentVariable("ContainerId", EnvironmentVariableTarget.Process);
+            string userId = req.Query["userId"];
+            log.LogInformation(string.Format("dbConn={0}, databaseId={1}, containerId={2}, userId={3}", dbConn, databaseId, containerId, userId));
 
-            string name = req.Query["name"];
+            // Param validation
+            if (string.IsNullOrEmpty(userId)) return new BadRequestObjectResult("A user Id was not provided"); // Bad request (400)
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // Connect to DB and execute query
+            List<object> items = new List<object>();
+            try
+            {
+                var dbClient = new CosmosClient(dbConn);
+                var dbContainer = dbClient.GetContainer(databaseId, containerId);
+                var dbQuery = string.Format("SELECT * FROM c WHERE c.userId = '{0}'", userId);
+                QueryDefinition queryDefinition = new QueryDefinition(dbQuery);
+                FeedIterator<object> queryResultSetIterator = dbContainer.GetItemQueryIterator<object>(queryDefinition);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                // Process the data set
+                while (queryResultSetIterator.HasMoreResults)
+                {
+                    FeedResponse<object> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                    foreach (var item in currentResultSet)
+                    {
+                        log.LogInformation(string.Format("item={0}", item));
+                        items.Add(item);
+                    }
+                }
+            }
+            catch (CosmosException ex)
+            {
+                log.LogError(ex.Message);
+                return new BadRequestObjectResult("Could not connect to the database."); // bad request (400)
+            }
 
+            // Process the results
+            if (items.Count == 0) return new NotFoundObjectResult("No ratings found for that user."); // Not found (404)
+            string responseMessage = JsonConvert.SerializeObject(items);
             return new OkObjectResult(responseMessage);
         }
     }
